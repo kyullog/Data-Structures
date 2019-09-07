38a7b466856988acc904224f74f080fbeb8029a0
Jonathan Dorety <kyullog@gmail.com>
2019-09-07 09:54:58 -0700

    Initial pass on queue

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,20 @@
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = []
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        self.storage.insert(0, item)
 
-  def len(self):
-    pass
+    def dequeue(self):
+        if self.len() < 1:
+            return
+        return self.storage.pop()
+
+    def len(self):
+        length = 0
+        for i in self.storage:
+            length += 1
+        return length
