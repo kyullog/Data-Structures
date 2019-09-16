0e2a66bad84dd926123d2e8fedfdba991da297f0
Jonathan Dorety <kyullog@gmail.com>
2019-09-16 14:44:57 -0700

    Add bubble_up, get_size, get_max

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,29 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        self.storage.append(value)
+        value_index = len(self.storage) - 1
+        self._bubble_up(value_index)
 
-  def delete(self):
-    pass
+    def delete(self):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        return self.storage[0]
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        return len(self.storage)
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        while index > 0:
+            parent_index = (index - 1) // 2
+            if self.storage[parent_index] < self.storage[index]:
+                self.storage[parent_index], self.storage[index] = self.storage[index], self.storage[parent_index]
+                index = parent_index
+            else:
+                break
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        pass
